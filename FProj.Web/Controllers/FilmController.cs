de57cf5948d5ac47b3b9ad1ba88b9d9df7e42dad
Bohdan Dorokhin <bogdan94@live.ru>
2017-05-14 12:42:34 +0300

    My films + Authorize + template fix

--- FProj.Web/Controllers/FilmController.cs
+++ FProj.Web/Controllers/FilmController.cs
@@ -29,6 +29,7 @@ namespace FProj.Web.Controllers
 
         public ActionResult Details(int Id) => View(UnitOfWork.Instance.FilmRepository.GetById(Id));
 
+        [Authorize]
         [HttpPost]
         public ActionResult UploadPoster(int Id, HttpPostedFileBase file, bool IsPoster = true)
         {
@@ -40,12 +41,23 @@ namespace FProj.Web.Controllers
             return Json(new { Ok = true, Path = WebConfigurationManager.AppSettings["ImageFolder"] + path });
         }
 
+        [Authorize]
         public ActionResult Create() => View(UnitOfWork.Instance.FilmRepository.Default());
 
+        [Authorize]
         [HttpPost]
         public ActionResult Create(FilmApi model, HttpPostedFileBase file, IEnumerable<HttpPostedFileBase> frames)
         {
+            if (file == null)
+            {
+                ModelState.AddModelError("poster", "hello");
+            }
+
+            if (!ModelState.IsValid) return View(model);
+
             model.DateCreated = DateTime.Now;
+            model.User = UnitOfWork.Instance.UserRepository.GetUserByEmail(User.Identity.Name);
+
             var film = UnitOfWork.Instance.FilmRepository.Create(model);
             if (file != null)
                 UploadPoster(film.Id, file);
@@ -65,6 +77,25 @@ namespace FProj.Web.Controllers
             }
 
             return RedirectToAction("Details", new { Id = film.Id });
+        }
+
+        [Authorize]
+        public ActionResult MyFilms()
+        {
+            var user = UnitOfWork.Instance.UserRepository.GetUserByEmail(User.Identity.Name);
+            var data = UnitOfWork.Instance.FilmRepository.GetPage(1, user: user);
+
+            return View("MyFilmList", data);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult GetMyPage(PageRequest request)
+        {
+            var user = UnitOfWork.Instance.UserRepository.GetUserByEmail(User.Identity.Name);
+            var data = request == null ? UnitOfWork.Instance.FilmRepository.GetPage(1, user: user) : UnitOfWork.Instance.FilmRepository.GetPage(request.PageNumber, request.CountPerPage, user);
+
+            return PartialView("FilmList", data.Data);
         }
     }
 }
